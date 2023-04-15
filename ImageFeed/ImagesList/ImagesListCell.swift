8eb11faa49f42a622428c4053b81b9bba837ea71
totoriole <t-punch@ya.ru>
2023-04-14 23:05:30 -0400

    New changes

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,11 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Toto Tsipun on 13.04.2023.
+//
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Toto Tsipun on 29.03.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configCell(for cell: ImagesListCell) { }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let imageListCell = cell as? ImagesListCell else {
+            print("Ошибка приведения типов")
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController {
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
