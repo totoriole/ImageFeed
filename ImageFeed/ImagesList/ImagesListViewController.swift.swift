90c30a3fe449661d13bfb5d4ffecc881124662bd
totoriole <t-punch@ya.ru>
2023-05-01 23:57:34 -0400

    Added navigation controller segue

--- ImageFeed/ImagesList/ImagesListViewController.swift.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift.swift
@@ -10,12 +10,24 @@ import UIKit
 class ImagesListViewController: UIViewController {
 
     private var listCell = ImagesListCell()
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
+    
     @IBOutlet weak private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: listCell.photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
@@ -37,6 +49,7 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Toto Tsipun on 30.04.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
