d7a0c1f0e38f59f787c2bcb640bc220e9b96dd2d
totoriole <t-punch@ya.ru>
2023-06-08 14:53:01 -0400

    Edited TabBarController

--- ImageFeed/TabBarController/TabBarController.swift
+++ ImageFeed/TabBarController/TabBarController.swift
@@ -14,9 +14,10 @@ final class TabBarController: UITabBarController {
         let storyBoard = UIStoryboard(name: "Main", bundle: .main)
         
         let imagesListViewController = storyBoard.instantiateViewController(withIdentifier: "ImagesListViewController")
-        
+//        let profileViewController = storyBoard.instantiateViewController(withIdentifier: "ProfileViewController")
         let profileViewController = ProfileViewController()
         profileViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_profile_active"), selectedImage: nil)
+        imagesListViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_editorial_active"), selectedImage: nil)
         
         self.viewControllers = [imagesListViewController, profileViewController]
     }
